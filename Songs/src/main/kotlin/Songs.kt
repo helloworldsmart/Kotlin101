55ad4894ea850c11f76c0a8c9f3cd391c0bb01f7
MichaelChen <helloworldsmart3@gmail.com>
2021-04-30 18:29:06 +0800

    P115

--- Songs/src/main/kotlin/Songs.kt
+++ Songs/src/main/kotlin/Songs.kt
@@ -15,7 +15,7 @@ fun main() {
     songTwo.stop()
     songThree.play()
 }
-class Dog(val name: String, var weight: Int, breed_param: String) {
+class Dog(val name: String, var weight_param: Int, breed_param: String) {
 
     init {
         println("Dog $name has been created.")
@@ -27,5 +27,17 @@ class Dog(val name: String, var weight: Int, breed_param: String) {
 
     init {
         println("The breed is $breed.")
+    }
+
+    var weight = weight_param
+        set(value) {
+            if (value > 0) field = value
+        }
+
+    val weightInKgs: Double
+        get() = weight / 2.2
+
+    fun bark() {
+        println(if (weight < 20) "Yip!" else "Woof!")
     }
 }
